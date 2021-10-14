e698bffd92046ec96b505ced87607138aa9d81f0
Gopal Krishan <gopalkbks@Gopals-MacBook-Pro.local>
2021-10-13 23:52:35 -0400

    first commit

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,70 +1,92 @@
-import React from 'react';
-
+import React, { useCallback, useReducer } from 'react';
 import Input from '../../shared/components/FormElements/Input';
-import Button from '../../shared/components/FormElements/Button';
-import {
-  VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH
-} from '../../shared/util/validators';
-import { useForm } from '../../shared/hooks/form-hook';
 import './PlaceForm.css';
+import {
+	VALIDATOR_MINLENGTH,
+	VALIDATOR_REQUIRE,
+} from '../../shared/util/validators';
+import Button from '../../shared/components/FormElements/Button';
+
+const formReducer = (state, action) => {
+	switch (action.type) {
+		case 'INPUT_CHANGE':
+			let formIsValid = true;
+			for (const inputId in state.inputs) {
+				if (inputId === action.inputId) {
+					formIsValid = formIsValid && action.isValid;
+				} else {
+					formIsValid = formIsValid && state.inputs[inputId].isValid;
+				}
+			}
+			return {
+				...state,
+				inputs: {
+					...state.inputs,
+					[action.inputId]: { value: action.value, isValid: action.isValid },
+				},
+				isValid: formIsValid,
+			};
+		default:
+			return state;
+	}
+};
 
 const NewPlace = () => {
-  const [formState, inputHandler] = useForm(
-    {
-      title: {
-        value: '',
-        isValid: false
-      },
-      description: {
-        value: '',
-        isValid: false
-      },
-      address: {
-        value: '',
-        isValid: false
-      }
-    },
-    false
-  );
+	const [formState, dispatch] = useReducer(formReducer, {
+		inputs: {
+			title: { value: '', isValid: false },
+			description: { value: '', isValid: false },
+			address: { value: '', isValid: false },
+		},
+		isValid: false,
+	});
+	const inputHandler = useCallback((id, value, isValid) => {
+		dispatch({
+			type: 'INPUT_CHANGE',
+			inputId: id,
+			value: value,
+			isValid: isValid,
+		});
+	}, []);
 
-  const placeSubmitHandler = event => {
-    event.preventDefault();
-    console.log(formState.inputs); // send this to the backend!
-  };
+	const submitHandler = (event) => {
+		event.preventDefault();
+		console.log(formState.inputs);
+	};
 
-  return (
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-      <Input
-        id="address"
-        element="input"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid address."
-        onInput={inputHandler}
-      />
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
-  );
+	return (
+		<div>
+			<form className="place-form" onSubmit={submitHandler}>
+				<Input
+					id="title"
+					element="input"
+					label="Title : "
+					validators={[VALIDATOR_REQUIRE()]}
+					onInput={inputHandler}
+					errorText="Please enter a valid title"
+				/>
+				<Input
+					id="description"
+					element="textarea"
+					label="Description : "
+					validators={[VALIDATOR_MINLENGTH(5)]}
+					onInput={inputHandler}
+					errorText="Please enter at least 5 characters"
+				/>{' '}
+				<Input
+					id="address"
+					element="textarea"
+					label="Address : "
+					validators={[VALIDATOR_MINLENGTH(5)]}
+					onInput={inputHandler}
+					errorText="Please enter at least 5 characters"
+				/>
+				<Button type="submit" disabled={!formState.isValid}>
+					SUBMIT
+				</Button>
+			</form>
+		</div>
+	);
 };
 
 export default NewPlace;
